a764c7a2a79f0b91eee11d0c4ecef4eb0c5ff884
Pablo Mercado <32917233+Pablo497@users.noreply.github.com>
2022-07-11 21:28:34 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <script src="https://www.mercadopago.com.co/integrations/v1/web-payment-checkout.js"
+data-preference-id="542346079-137fb203-490b-43f2-b926-3dfe78c1eb25" data-source="button">
+</script>
+
+</body>
+</html>
